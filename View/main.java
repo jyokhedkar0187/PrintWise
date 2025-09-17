d4130f5f0b6a63606332ed5685838692367ab479
tanu2474 <ahertanvi462@gmail.com>
2025-09-17 15:24:06 +0530

    main file code added

--- View/main.java
+++ View/main.java
@@ -1,1 +1,29 @@
-package View;
+package com.projectf;
+
+import com.projectf.Controller.MainApp;
+
+//import com.projectf.Controller.MainApp;
+//import com.projectf.view.AdminFeedback;
+//import com.projectf.view.AdminHome;
+//import com.projectf.Controller.MainApp;
+//import com.projectf.view.adminprofile;
+//import com.projectf.Controller.logincontroller;
+//import com.projectf.view.LoginView;
+//import com.projectf.view.FAQP;
+//import com.projectf.view.Homepage;
+//import com.projectf.view.Homepage;
+//import com.projectf.view.LoginView;
+//import com.projectf.view.adminfeedback;
+//import com.projectf.view.AdminHome;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Application.launch(MainApp.class, args);
+}
+}
+
+
+
